2f844e40dbdaaf1f9b6eff45f8c9065f2153027f
Fourt14n <hebertsep1914@gmail.com>
2025-01-19 10:15:51 -0300

    Adicionar arquivos de projeto.

---
+++ ToDoList/Entities/EMenu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Entities
+{
+    internal enum EShowMenu
+    {
+        // Enum for the menu options
+        // Setted the value to start from 1
+        Add = 1,
+        Listing,
+        Excluding,
+        Editing,
+        Exiting,
+        Returning
+
+    }
+}
---
+++ ToDoList/Program.cs
@@ -0,0 +1,11 @@
+﻿using ToDoList.Entities;
+namespace ToDoList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.InitialMenu(); 
+        }
+    }
+}
